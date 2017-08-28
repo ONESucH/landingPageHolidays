fd517da05913fc172e6f34747e94caefd1aedc1b
Onesuch07 <onesuch07@gmail.com>
2017-08-28 15:03:15 +0300

    add one window

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
 'use strict';
+var booleanAnketa = false;
+
+function anketa() {
+    booleanAnketa = !booleanAnketa;
+
+    if(booleanAnketa) {
+        console.log('Открыто');
+    } else {
+        console.log('Закрыто');
+    }
+
+}
